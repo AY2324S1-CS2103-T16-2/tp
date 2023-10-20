d5ed25a2227659964f53f5ae1a78884561344f14
Eric <110958646+EricXiong420@users.noreply.github.com>
2023-10-20 16:06:08 +0800

    Revert "Add shift command"

--- src/main/java/seedu/address/logic/parser/ParserUtil.java
+++ src/main/java/seedu/address/logic/parser/ParserUtil.java
@@ -5,7 +5,6 @@ import static java.util.Objects.requireNonNull;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.util.StringUtil;
@@ -123,18 +122,4 @@ public class ParserUtil {
         }
         return affiliationSet;
     }
-
-    /**
-     * Parses a {@code String shiftDayString} into a {@code ShiftDays}.
-     * Leading and trailing whitespaces will be trimmed.
-     *
-     * @throws ParseException if the given {@code shiftDayString} is invalid.
-     */
-    public static Set<Integer> parseShiftDays(String shiftDayString) {
-        requireNonNull(shiftDayString);
-        return shiftDayString.trim().chars().map(x -> x - '0') // converts string into CharStream, then into IntStream
-                .boxed().collect(Collectors.toSet());
-    }
-
-
 }
